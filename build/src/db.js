35cfc198ab84278b9cb313635fd98007aab0ccfc
gozipizi <quentin.gourdel@ensiie.fr>
2024-03-22 23:10:23 +0100

    Stop tracking build directory

--- build/src/db.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const client_1 = require("@prisma/client");
-const Environment_1 = require("./utils/Environment");
-const globalForPrisma = global;
-const prisma = globalForPrisma.prisma || new client_1.PrismaClient({ log: ['query'] });
-if ((0, Environment_1.isProdEnvironment)())
-    globalForPrisma.prisma = prisma;
-exports.default = prisma;
