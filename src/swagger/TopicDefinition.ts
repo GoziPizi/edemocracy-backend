6d3fa2dda3b2b6d01545d071d23c2358d1a3e9e3
gozipizi <quentin.gourdel@ensiie.fr>
2024-04-30 15:19:42 +0200

    swagger

--- src/swagger/TopicDefinition.ts
+++ src/swagger/TopicDefinition.ts
@@ -2,6 +2,7 @@ import { TopicOutputDto } from "@/types/dtos/TopicDtos";
 
 export const TopicOutputDefinition: TopicOutputDto = {
     id: '1',
+    userId: '1',
     title: 'string',
     description: 'string',
     parentTopicId: '2',
