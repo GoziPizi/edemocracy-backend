5abf4ae5b7d47f8515f25368d446cea8b741acbb
gozipizi <quentin.gourdel@ensiie.fr>
2024-08-06 16:40:57 +0200

    Obtention

--- src/repositories/PreRegistrationRepository.ts
+++ src/repositories/PreRegistrationRepository.ts
@@ -4,8 +4,15 @@ import { StandardUserCreateInputDto } from "@/types/dtos/UserDto";
 class PreRegistrationRepository extends PrismaRepository {
 
     createPreRegistration = async (user : StandardUserCreateInputDto) => {
+        let finalUser: any = { ...user }
+        if(user.formationObtention) {
+            finalUser = {
+                ...finalUser,
+                formationObtention: Number(user.formationObtention)
+            }
+        }
         const preRegistration = await this.prismaClient.preRegistrationUser.create({
-            data: user
+            data: finalUser
         })
         return preRegistration
     }
