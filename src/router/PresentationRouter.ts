b561bf7381db708648bc1cf2bba24de30b3ee24d
gozipizi <quentin.gourdel@ensiie.fr>
2024-10-15 16:19:44 +0200

    Modif par l'admin

--- src/router/PresentationRouter.ts
+++ src/router/PresentationRouter.ts
@@ -1,3 +1,5 @@
+import { JwtNotInHeaderException } from '@/exceptions/JwtExceptions';
+import AuthentificationService from '@/services/AuthentificationService';
 import express, { NextFunction, Request, Response } from 'express';
 import { promises as fs } from 'fs';
 
@@ -30,6 +32,35 @@ PresentationRouter.get('/founder', async (req: Request, res: Response, next: Nex
     try {
         const data = await fs.readFile('src/utils/presentation/founder', 'utf-8');
         res.status(200).send({content: data});
+    } catch (error) {
+        next(error);
+    }
+});
+
+PresentationRouter.put('/', async (req: Request, res: Response, next: NextFunction) => {
+    /**
+        #swagger.tags = ['Presentation']
+        #swagger.description = 'Endpoint to update the main presentation'
+        #swagger.parameters['content'] = { description: 'Main presentation content' }
+        #swagger.responses[200] = {
+            description: 'Main presentation updated',
+        }
+     */
+    try {
+        //Check if the user is an admin
+        const token = req.headers.authorization;
+        if(!token) {
+            throw new JwtNotInHeaderException();
+        }
+        const role = await AuthentificationService.getUserRole(token);
+        if(role !== 'ADMIN') {
+            throw new Error('You are not allowed to do this');
+        }
+        const presentation = req.body.presentation;
+        const founder = req.body.founder;
+        await fs.writeFile('src/utils/presentation/presentation', presentation);
+        await fs.writeFile('src/utils/presentation/founder', founder);
+        res.status(200).send();
     } catch (error) {
         next(error);
     }
